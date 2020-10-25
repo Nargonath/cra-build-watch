6919ede389b06508c3aa3887f311effebe4861e5
Tomi Tavela <tomi.tavela@gmail.com>
2020-10-25 12:28:01 +0200

    disabled fast-refresh

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 process.env.NODE_ENV = 'development'; // eslint-disable-line no-process-env
+process.env.FAST_REFRESH = false;
 
 const importCwd = require('import-cwd');
 const fs = require('fs-extra');
